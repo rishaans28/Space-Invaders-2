c3de8404be4e5b8a8a5824397ee5d9d4db59fd32
rishaans28 <rishaansoni28@gmail.com>
2025-04-23 20:08:58 -0700

    Update sprites.py

--- Code/sprites.py
+++ Code/sprites.py
@@ -189,3 +189,18 @@ class Star(pygame.sprite.Sprite):
         self.rotation = randint(-20,20)
         self.image = pygame.transform.rotate(self.image, self.rotation)
         self.rect = self.image.get_frect(center = (randint(0, WINDOW_WIDTH), randint(0, WINDOW_HEIGHT)))
+
+class Explosion(pygame.sprite.Sprite):
+    def __init__(self, groups, frames, pos):
+        super().__init__(groups)
+        self.frames = frames
+        self.frame_index = 0
+        self.image = self.frames[self.frame_index]
+        self.rect = self.image.get_frect(center = pos)
+    
+    def update(self, dt):
+        self.frame_index += 20 * dt
+        if self.frame_index >= len(self.frames):
+            self.kill()
+        else:
+            self.image = self.frames[int(self.frame_index) % len(self.frames)]
